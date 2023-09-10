68aa06920c9dcec5891b7222dd0c3ad912f4f7ac
Lizzie Siegle <esiegle@brynmawr.edu>
2023-09-10 00:54:40 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import Replicate
+from langchain.memory import ConversationBufferWindowMemory
+from twilio.twiml.messaging_response import MessagingResponse
+
+template = """Assistant is a large language model.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist. 
+
+I want you to act as Klay Thompson giving advice and answering questions. You will reply with what he would say.
+SMS: {sms_input}
+Assistant:"""
+
+prompt = PromptTemplate(input_variables=["sms_input"], template=template)
+
+sms_chain = LLMChain(
+    llm = Replicate(model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"), 
+    prompt=prompt,
+    memory=ConversationBufferWindowMemory(k=2),
+    llm_kwargs={"max_length": 4096}
+)
+app = Flask(__name__)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms():
+    resp = MessagingResponse()
+    inb_msg = request.form['Body'].lower().strip()
+    output = sms_chain.predict(sms_input=inb_msg)
+    print(output)
+    resp.message(output)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
